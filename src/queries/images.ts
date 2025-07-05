9ef8bb9550b9eadfd2b2f9ff14d2b1a44bb7e399
chris abbott <chris.abs96@gmail.com>
2025-07-05 21:41:20 +0100

    feat: dummy image data - temp

--- src/queries/images.ts
+++ src/queries/images.ts
@@ -2,21 +2,24 @@ import { useQuery } from "@tanstack/react-query";
 import { useLocation } from "@tanstack/react-router";
 
 import { imageQueries } from "@/api/images";
+import { mockImages } from "@/lib/mock-data/images";
 import { getImageCategoryFromRoute } from "@/types/images";
 
 export function useProductImages() {
   const location = useLocation();
   const category = getImageCategoryFromRoute(location.pathname);
 
-  return useQuery(
-    category
-      ? imageQueries.byCategory(category)
-      : {
-          queryKey: ["images", "disabled"],
-          queryFn: () => Promise.resolve([]),
-          enabled: false,
-        }
-  );
+  return useQuery({
+    queryKey: ["images", category],
+    queryFn: () => {
+      return Promise.resolve(category ? mockImages[category] || [] : []);
+    },
+    // TODO: when the BE is ready for image fetching
+    // queryFn: () => fetchImagesByCategory(category)
+    enabled: Boolean(category),
+    staleTime: 5 * 60 * 1000,
+    gcTime: 10 * 60 * 1000,
+  });
 }
 
 export function useProductImagesForCategory(category: string) {
